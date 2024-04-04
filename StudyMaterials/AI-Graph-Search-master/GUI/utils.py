6fe3e1c9b99ce52ebbd1fe93fe0637cdc7334505
yxu149 <yxu149@ucsc.edu>
2024-04-03 20:54:03 -0700

    Added some notes

--- StudyMaterials/AI-Graph-Search-master/GUI/utils.py
+++ StudyMaterials/AI-Graph-Search-master/GUI/utils.py
@@ -1,4 +1,18 @@
+"""
+Utilities
+
+Handle  - OverlapException 
+        - DuplicateConnectionException. 
+Define  - Mouse_state. 
+"""
+
 class OverlapException(Exception):
+    """
+    Parameters: Exception
+    Note: Mouse position overlaps an existing node. 
+
+    Potential for a delete node function. 
+    """
 
     def __init__(self,message="Can't place the node as it overlaps with another node"
                 ,title="Overlaping Error"):
@@ -10,6 +24,12 @@ class OverlapException(Exception):
         return self.message
 
 class DuplicateConnectionException(Exception):
+    """
+    Parameters: Exception
+    Note: Duplicate paths between two nodes. 
+
+    Potential for a delete connection function. 
+    """
 
     def __init__(self,message="This connection already exists"
                 ,title="Duplicate Connection Error"):
@@ -22,6 +42,9 @@ class DuplicateConnectionException(Exception):
 
 
 class Mouse_state:
+    """
+    Define: mouse states, auto set their UI icons. 
+    """
    
     normal  = 1          # neither circle nor line is clicked 
     circle = 2           # circle is clicked
@@ -42,6 +65,12 @@ class Mouse_state:
         self.callback = callback
 
     def __reset_cursor(self):
+        """
+        Parameters: self
+
+        Checks self.__state and reassign corresponding 
+        values by calling self.__root.config(cursor="") 
+        """
         if (self.__state == Mouse_state.goal_node):
             self.__root.config(cursor="target")
         elif(self.__state == Mouse_state.initial_node):
@@ -53,9 +82,13 @@ class Mouse_state:
         else:
             self.__root.config(cursor="arrow")
             
-
     def set_state(self,new_state):
-        
+        """
+        Parameters: self, new_state
+
+        Sets self.__state = new_state when safe. 
+        Then resets cursor. 
+        """
         if self.callback :
             self.callback()
             self.__state = new_state
@@ -64,7 +97,12 @@ class Mouse_state:
         self.__reset_cursor()
 
     def get_state(self):
+        """
+        Parameters: self
+        Returns: self.__state
         
+        Access function. 
+        """
         return self.__state
 
 mouse = Mouse_state()
