12a2674a060c2af023aea5ce0c2a5891b56db498
yxu149 <yxu149@ucsc.edu>
2024-04-18 21:01:47 -0700

    some settings file

---
+++ Graph/Settings/Settings.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.__str__()
+
+"""
+Apparences
+"""
+"""
+Node
+"""
+NODE_RADIUS = 20
+NODE_COLOR = '#FFFFFF'
+NODE_OUTLINE_THICKNESS = 2
+NODE_OUTLINE_COLOR = '#000000'
+NODE_TEXT_COLOR = '#000000'
+NODE_TEXT_FONT = ''
+"""
+Line
+"""
+LINE_THICKNESS = 2
+LINE_COLOR = '#000000'
+LINE_TEXT_COLOR = '#000000'
+LINE_TEXT_FONT = ''
+"""
+Canvas / Backdrop
+"""
+CANVAS_BG_COLOR = 'grey'
+"""
+Pathfinding
+"""
+NODE_SELECTED = 'orange'
+NODE_BEGIN_COLOR = 'red'
+NODE_END_COLOR = 'green'
+NODE_HERE_COLOR = 'cyan'
+NODE_VISITED_COLOR = 'grey'
+
+PATH_LINE_THICKNESS = 2
+PATH_LINE_COLOR = ''
+PATH_NODE_COLOR = 'green'
+"""
+Others
+"""
+
---
+++ Graph/main.py
@@ -0,0 +1,4 @@
+import sys
+from Settings.Settings import * 
+
+sys.path.append(BASE_DIR)
