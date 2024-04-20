244b7e2adba7957d119381fe3ebd04a9a5702cd9
yxu149 <yxu149@ucsc.edu>
2024-04-19 23:15:30 -0700

    some wiring done

--- Graph/GUI/parts/cursor.py
+++ Graph/GUI/parts/cursor.py
@@ -2,3 +2,24 @@
 Defines mouse states
 Coordinates mouse functions
 """
+class Mouse_States: 
+    """
+    Define: Mouse states
+    Function: 
+        Sets cursor icons
+        Sets cursor modes 
+    """
+    # Panel S0
+    state_entrance = 1
+    state_exit = 2 
+    state_weight = 3 
+    state_04 = 4
+    state_add = 5
+    state_sub = 6
+    state_path = 7
+    state_08 = 8
+    
+    # Panel S1
+    state_reset = 14
+
+    # Panel S2 and Panel S3 not wired
