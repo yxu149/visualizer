1d4a02597505b12156668d5dde55f98ab7cee9f3
yxu149 <yxu149@ucsc.edu>
2024-05-10 19:33:39 -0700

    treemakers

---
+++ Grapher/gui/treemakers/treecanvas.py
@@ -0,0 +1,33 @@
+import tkinter as tk 
+import tkinter.constants as constants
+import settings.settings as settings 
+
+class TreeCanvas(tk.Frame): 
+    def __init__(self, root, w=100, h=10000): 
+        tk.Frame.__init__(self, root) 
+        self.node_count = 0
+        self.h_scrollbar = tk.Scrollbar(self, orient=constants.HORIZONTAL) 
+        self.v_scrollbar = tk.Scrollbar(self, orient=constants.VERTICAL)
+        self.canvas = tk.Canvas(self, background=settings.STAGE_BG, scrollregion=(0,0,w,h), \
+                                yscrollcommand=self.v_scrollbar.set,\
+                                xscrollcommand=self.h_scrollbar.set)
+        self.h_scrollbar['command'] = self.canvas.xview 
+        self.v_scrollbar['command'] = self.canvas.yview
+        self.pack() 
+    
+    def pack(self): 
+        self.canvas.grid(row=0,column=0,sticky="NSEW")
+        self.h_scrollbar.grid(row=1,column=0,sticky="EW")
+        self.v_scrollbar.grid(row=0,column=1,sticky="NS")
+        self.rowconfigure(0,weight=1)
+        self.columnconfigure(0,weight=1) 
+    
+if __name__=="__main__":
+    root = tk.Tk()
+    can = TreeMaker(root)
+
+    can.grid(row=0,column=0,sticky=("NSEW"))
+    root.rowconfigure(0,weight=1)
+    root.columnconfigure(0,weight=1)
+
+    root.mainloop()
--- Grapher/gui/wirings/cursor_ctrl.py
+++ Grapher/gui/wirings/cursor_ctrl.py
@@ -1,19 +1,22 @@
+# Some state definitions
+
+default = 0
+
+add_node = 1
+remove_node = 2
+    
+add_line = 3 
+remove_line = 4
+
+start_node = 5
+end_node = 6 
+
+disabled = -1 
+
 class CursorStates(): 
     """
     Define: Cursor states, set their UI icons
     """
-    default = 0
-
-    add_node = 1
-    remove_node = 2
-    
-    add_line = 3 
-    remove_line = 4
-
-    start_node = 5
-    end_node = 6 
-
-    disabled = -1 
 
     def __init__(self): 
         self.__state = CursorStates.default 
@@ -26,11 +29,10 @@ class CursorStates():
     def set_callback(self, callback): 
         self.callback = callback 
 
-    def reset(self): 
-        """
-        Parameters: self 
+    def set_icon(self): 
+        pass
 
-        Checks self.__state and reassign corresponding 
-        values by calling self.__root.config(cursor="")
-        """
-        if (self.__state == )
+    def get_state(self): 
+        return self.__state 
+    
+mouse = CursorStates() 
